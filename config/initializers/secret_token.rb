5980c09ffe9ff3e351a5fc9a2eccbbb4e66f92ed
Matthew Sumner <matt.m.sumner@gmail.com>
2013-11-25 11:56:13 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WordSquare::Application.config.secret_key_base = 'b1dc3ebcc8f98b15197272642bbb988dd425abb3fba5945c495ea2149ebb73ee7084fdabf58095cccc9debbfd0c4aaee76cd0c03df67239e2995c8e8a3868bf1'
