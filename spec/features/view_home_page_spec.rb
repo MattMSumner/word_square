ca3d131af13771ee736c8b771b8c239d9aee09fa
Matthew Sumner <matt.m.sumner@gmail.com>
2013-11-26 11:18:03 -0700

    test confirming app name change to words

--- spec/features/view_home_page_spec.rb
+++ spec/features/view_home_page_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature 'view the homepage' do
   scenario 'user sees relevant information' do
     visit root_path
-    expect(page).to have_title 'WordSquare'
+    expect(page).to have_title 'Words'
     expect(page).to have_css '[data-role="description"]'
   end
 end
