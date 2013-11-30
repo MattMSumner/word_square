63ecb8784f438d080ff6cf2781e8c9b23ba22585
Matthew Sumner <matt.m.sumner@gmail.com>
2013-11-30 10:38:30 -0700

    refactor square_searcher into smaller methods

--- app/models/square_searcher.rb
+++ app/models/square_searcher.rb
@@ -17,15 +17,7 @@ class SquareSearcher
 
   def generate_word_square
     choose_random_starting_word
-    while @words.length < @dimension do
-      if no_more_starting_words
-        break
-      elsif available_words.present?
-        @words << available_words.sample
-      else
-        remove_last_word_and_add_to_tried(@words.length)
-      end
-    end
+    loop_to_find_words
   end
 
   def choose_random_starting_word
@@ -33,26 +25,54 @@ class SquareSearcher
     @words = @words.compact
   end
 
+  def loop_to_find_words
+    while @words.length < @dimension do
+      if no_more_starting_words
+        break
+      else
+        find_a_word
+      end
+    end
+  end
+
   def no_more_starting_words
     @words.empty? && available_words.empty?
+  end
+
+  def find_a_word
+    if available_words.present?
+      add_word
+    else
+      remove_last_word_and_add_to_tried(@words.length)
+    end
+  end
+
+  def available_words(word_number = @words.length)
+    if @tried[word_number + 1]
+      pool(word_number) - @tried[word_number + 1]
+    else
+      pool(word_number)
+    end
+  end
+
+  def pool(integer)
+    @lexicon_trie.wildcard(string(integer))
+  end
+
+  def string(integer)
+    prefix(integer) + '*' * (@dimension - integer)
+  end
+
+  def prefix(integer)
+    @words.map { |word| word[integer] }.join('')
+  end
+
+  def add_word
+    @words << available_words.sample
   end
 
   def remove_last_word_and_add_to_tried(index)
     @tried[index] ||= []
     @tried[index] << @words.pop
-  end
-
-  def available_words
-    prefix = @words.map { |word| word[@words.length] }.join('')
-
-    string = prefix + '*' * (@dimension - @words.length)
-
-    pool = @lexicon_trie.wildcard(string)
-
-    if @tried[@words.length + 1]
-      pool = pool - @tried[@words.length + 1]
-    end
-
-    pool
   end
 end
